d927e443403af4c1fc9dfcd3cd2652a1ce6e0294
Bambang Kurniawan <bembkurniawan@gmail.com>
2017-10-29 19:52:08 +0700

    modify todo model: erase done_at, set default to created_at and done

--- models/index.js
+++ models/index.js
@@ -7,4 +7,4 @@ const mongoose = require('mongoose');
 mongoose.connect(`mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@ds053894.mlab.com:53894/todo-fancy`);
 mongoose.Promise = global.Promise;
 
-module.exports = { FbUser };
+module.exports = { FbUser, Todo };
