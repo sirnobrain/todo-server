50692697bb8b33f09402274bc581267445336fbc
Bambang Kurniawan <bembkurniawan@gmail.com>
2017-10-29 17:57:16 +0700

    create generate-token helper

--- controllers/user.js
+++ controllers/user.js
@@ -2,16 +2,21 @@
 
 const models = require('./../models');
 const genResponse = require('./../helpers/generate-response');
+const genJwtoken = require('./../helpers/generate-jwtoken');
 
 class User {
 	static signin(req, res) {
 		models.FbUser.getNameAndId(req.headers.fb_token, req.headers.fb_id)
-		.then(nameAndId => {
-			const resp = genResponse(200, 'get fb user name and id', nameAndId, null);
+		.then(FbNameAndId => {
+			return genJwtoken(FbNameAndId);
+		})
+		.then(jwtoken => {
+			const data = {jwtoken};
+			const resp = genResponse(200, 'retireve token from server', data, null);
 			res.status(200).send(resp);
 		})
 		.catch(err => {
-			const resp = genResponse(500, 'failed to get fb user name and id', null, err);
+			const resp = genResponse(500, 'failed to retrieve token from server', null, err);
 			res.status(500).send(resp);
 		});
 	}
