0a66438377a06d02d417ac72bfe656f32849598c
Bambang Kurniawan <bembkurniawan@gmail.com>
2017-10-29 17:13:15 +0700

    create fb-user model

---
+++ models/fb-user.js
@@ -0,0 +1,24 @@
+'use strict'
+
+const FB = require('fb');
+
+const fb = new FB.Facebook({
+	appId: '167628840487134',
+	appSecret: process.env.FB_APP_SECRET,
+	version: 'v2.10'
+});
+
+class FbUser {
+	static getNameAndId(fbToken, fbId) {
+		return new Promise((resolve, reject) => {
+			FB.api('me', {fields: ['id', 'name'], access_token: fbToken }, nameAndId => {
+				console.log(nameAndId);
+				if (nameAndId.id === fbId) resolve(nameAndId);
+				else reject('Token and Id Do Not Match');
+			});
+		});
+	}
+}
+
+
+module.exports = FbUser;
