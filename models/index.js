e015c2487e1841b20f1217c8fd261b2bf234462e
Bambang Kurniawan <bembkurniawan@gmail.com>
2017-11-12 06:49:35 +0700

    change a lot

--- models/index.js
+++ models/index.js
@@ -4,7 +4,9 @@ const FbUser = require('./fb-user');
 const Todo = require('./todo');
 
 const mongoose = require('mongoose');
-mongoose.connect(`mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@ds053894.mlab.com:53894/todo-fancy`);
+const URI = `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@ds053894.mlab.com:53894/todo-fancy`;
+const options = { useMongoClient: true }
+mongoose.connect(URI, options);
 mongoose.Promise = global.Promise;
 
 module.exports = { FbUser, Todo };
